7da3ac0e4b368539c6548e6b8ceb996b0367e544
bigsparsh <sparshsingh7586@gmail.com>
2024-01-19 12:52:43 +0530

    Added getspecific and CRUD for todo BE

--- backend/routes/todo.js
+++ backend/routes/todo.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const router = Router();
+const zod = require("zod");
 
 const { Todo } = require("../db/");
 
@@ -10,6 +11,86 @@ router.get("/getall", async (req, res) => {
 	});
 });
 
-router.post("/create", async());
+router.post("/create", async (req, res) => {
+	const todoSchema = zod.object({
+		title: zod.string(),
+		desc: zod.string(),
+		assocDate: zod.date(),
+	});
+	const currentTimeStamp = new Date();
+	if (todoSchema.safeParse(req.body).success) {
+		const todo = req.body;
+		const newTodo = new Todo({
+			title: todo.title,
+			desc: todo.desc,
+			assocDate: todo.assocDate,
+			timestamp: currentTimeStamp,
+		});
+		await newTodo.save();
+	} else {
+		res.status(400).json({
+			error: "INVAILD BODY FORMAT",
+		});
+		return;
+	}
+	res.json({
+		message: "Todo created Successfully",
+	});
+});
+
+router.post("/getspecific", async (req, res) => {
+	const todoId = req.query.id;
+	if (todoId) {
+		res.json({
+			todo: await Todo.findById(todoId),
+		});
+		return;
+	}
+	res.status(400).json({
+		error: "TODO ID NOT FOUND IN QUERY",
+	});
+});
+
+router.post("/delete", async (req, res) => {
+	const todoId = req.query.id;
+	await Todo.findByIdAndDelete(todoId);
+	if (todoId) {
+		res.json({
+			message: "Todo deleted successfully",
+		});
+		return;
+	}
+	res.status(400).json({
+		error: "TODO ID NOT FOUND IN QUERY",
+	});
+});
+
+router.post("/update", async (req, res) => {
+	const todoSchema = zod.object({
+		title: zod.string(),
+		desc: zod.string(),
+		assocDate: zod.date(),
+	});
+	const todoId = req.query.id;
+	const currentTimeStamp = new Date();
+	if (todoSchema.safeParse(req.body).success) {
+		if (todoId) {
+			const todo = req.body;
+			todo.timestamp = currentTimeStamp;
+			await Todo.findByIdAndUpdate(todoId, todo);
+			res.json({
+				message: "Todo updated successfully",
+			});
+			return;
+		}
+		res.status(400).json({
+			error: "TODO ID NOT FOUND IN QUERY",
+		});
+		return;
+	}
+	res.status(400).json({
+		error: "INVAILD BODY FORMAT",
+	});
+});
 
 module.exports = router;
